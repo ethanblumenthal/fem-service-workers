50b7411f1c442a102790401257c07404def2a110
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-07-31 11:46:36 -0500

    Created web worker to generate fibonacci sequence.

--- web-worker-exercise/web/js/home.js
+++ web-worker-exercise/web/js/home.js
@@ -1,49 +1,52 @@
-(function Home(){
-	"use strict";
+(function Home() {
+  'use strict';
 
-	var startStopBtn;
-	var fibsList;
+  var startStopBtn;
+  var fibsList;
+  var worker;
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+  document.addEventListener('DOMContentLoaded', ready, false);
 
+  // **********************************
 
-	// **********************************
+  function ready() {
+    startStopBtn = document.getElementById('start-stop-btn');
+    fibsList = document.getElementById('fibs');
 
-	function ready() {
-		startStopBtn = document.getElementById("start-stop-btn");
-		fibsList = document.getElementById("fibs");
+    startStopBtn.addEventListener('click', startFibs, false);
+  }
 
-		startStopBtn.addEventListener("click",startFibs,false);
-	}
+  function renderFib(num, fib) {
+    var p = document.createElement('div');
+    p.innerText = `Fib(${num}): ${fib}`;
+    if (fibsList.childNodes.length > 0) {
+      fibsList.insertBefore(p, fibsList.childNodes[0]);
+    } else {
+      fibsList.appendChild(p);
+    }
+  }
 
-	function renderFib(num,fib) {
-		var p = document.createElement("div");
-		p.innerText = `Fib(${num}): ${fib}`;
-		if (fibsList.childNodes.length > 0) {
-			fibsList.insertBefore(p,fibsList.childNodes[0]);
-		}
-		else {
-			fibsList.appendChild(p);
-		}
-	}
+  function startFibs() {
+    startStopBtn.removeEventListener('click', startFibs, false);
+    startStopBtn.addEventListener('click', stopFibs, false);
 
-	function startFibs() {
-		startStopBtn.removeEventListener("click",startFibs,false);
-		startStopBtn.addEventListener("click",stopFibs,false);
+    startStopBtn.innerText = 'Stop';
+    fibsList.innerHTML = '';
 
-		startStopBtn.innerText = "Stop";
-		fibsList.innerHTML = "";
+    worker = new Worker('/js/worker.js');
+    worker.addEventListener('message', onMessage);
+    worker.postMessage({ start: true });
+  }
 
-		// TODO
-	}
+  function stopFibs() {
+    startStopBtn.removeEventListener('click', stopFibs, false);
+    startStopBtn.addEventListener('click', startFibs, false);
+    startStopBtn.innerText = 'Start';
 
-	function stopFibs() {
-		startStopBtn.removeEventListener("click",stopFibs,false);
-		startStopBtn.addEventListener("click",startFibs,false);
+    worker.terminate();
+  }
 
-		startStopBtn.innerText = "Start";
-
-		// TODO
-	}
-
+  function onMessage(evt) {
+    renderFib(evt.data.idx, evt.data.fib);
+  }
 })();
