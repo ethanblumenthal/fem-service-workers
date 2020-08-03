8654637e567c17cb9a42a0172c5c1fc8fef63761
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-08-03 12:38:47 -0500

    Created site cache using a service worker.

--- service-worker-exercise/web/js/sw.js
+++ service-worker-exercise/web/js/sw.js
@@ -1,8 +1,27 @@
 'use strict';
 
-const version = 2;
-isOnline = true;
-isLoggedIn = false;
+const version = 4;
+var isOnline = true;
+var isLoggedIn = false;
+var cacheName = `ramblings-${version}`;
+
+var urlsToCache = {
+  loggedOut: [
+    '/',
+    '/about',
+    '/contact',
+    'login',
+    '/404',
+    '/offline',
+    '/js/blog.js',
+    '/js/home.js',
+    '/js/login.js',
+    '/js/add-post.js',
+    '/css/style.css',
+    '/images/logo.gif',
+    '/images/offline.png',
+  ],
+};
 
 self.addEventListener('install', onInstall);
 self.addEventListener('activate', onActivate);
@@ -12,6 +31,7 @@ main().catch(console.error);
 
 async function main() {
   await sendMessage({ requestStatusUpdate: true });
+  await cacheLoggedOutFiles();
 }
 
 async function onInstall(evt) {
@@ -42,6 +62,52 @@ function onActivate(evt) {
 }
 
 async function handleActivation() {
+  await clearCaches();
+  await cacheLoggedOutFiles(true);
   await clients.claim();
   console.log(`Service worker ${version} is activated.`);
+}
+
+async function clearCaches() {
+  var cacheNames = await caches.keys();
+  var oldCacheNames = cacheNames.filter(function matchOldCache(cacheName) {
+    var [, cacheNameVersion] = cacheName.match(/^ramblings-(\d+)$/) || [];
+    cacheNameVersion =
+      cacheNameVersion != null ? Number(cacheNameVersion) : cacheNameVersion;
+    return cacheNameVersion > 0 && version !== cacheNameVersion;
+  });
+  await Promise.all(
+    oldCacheNames.map(function deleteCache(cacheName) {
+      return caches.delete(cacheName);
+    })
+  );
+}
+
+async function cacheLoggedOutFiles(forceReload = false) {
+  var cache = await caches.open(cacheName);
+
+  return Promise.all(
+    urlsToCache.loggedOut.map(async function requestFile(url) {
+      try {
+        let res;
+
+        if (!forceReload) {
+          res = await cache.match(url);
+          if (res) {
+            return;
+          }
+        }
+
+        let fetchOptions = {
+          method: 'GET',
+          cache: 'no-store',
+          credentials: 'omit',
+        };
+        res = await fetch(url, fetchOptions);
+        if (res.ok) {
+          return cache.put(url, res);
+        }
+      } catch (err) {}
+    })
+  );
 }
