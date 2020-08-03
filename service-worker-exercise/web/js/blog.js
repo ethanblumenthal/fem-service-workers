09030bac0c12c578f83194b3d96d676a95ae28e3
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-08-03 12:16:06 -0500

    Created first service worker.

--- service-worker-exercise/web/js/blog.js
+++ service-worker-exercise/web/js/blog.js
@@ -1,16 +1,79 @@
-(function Blog(){
-	"use strict";
+(function Blog() {
+  'use strict';
 
-	var offlineIcon;
-	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+  var offlineIcon;
+  var isOnline = 'onLine' in navigator ? navigator.onLine : true;
+  var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '');
+  var usingSW = 'serviceworker' in navigator;
+  var swRegistration;
+  var svcworker;
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+  document.addEventListener('DOMContentLoaded', ready, false);
 
+  initServiceWorker().catch(console.error);
 
-	// **********************************
+  // **********************************
 
-	function ready() {
-		offlineIcon = document.getElementById("connectivity-status");
-	}
+  function ready() {
+    offlineIcon = document.getElementById('connectivity-status');
 
+    if (!isOnline) {
+      offlineIcon.classList.remove('hidden');
+    }
+
+    window.addEventListener('online', function online() {
+      offlineIcon.classList.add('hidden');
+      isOnline = true;
+      sendStatusUpdate();
+    });
+
+    window.addEventListener('offline', function offline() {
+      offlineIcon.classList.remove('hidden');
+      isOnline = false;
+      sendStatusUpdate();
+    });
+  }
+
+  async function initServiceWorker() {
+    swRegistration = await navigator.serviceWorker.register('/sw.js', {
+      updateViaCache: 'none',
+    });
+
+    svcworker =
+      swRegistration.installing ||
+      swRegistration.waiting ||
+      swRegistration.active;
+
+    navigator.serviceWorker.addEventListener(
+      'controllerchange',
+      function onController() {
+        svcworker = navigator.serviceWorker.controller;
+        sendStatusUpdate(svcworker);
+      }
+    );
+
+    navigator.serviceWorker.addEventListener('message', onSWMessage);
+  }
+
+  function onSWMessage(evt) {
+    var { data } = evt;
+    if (data.requestStatusUpdate) {
+      console.log('Received status update request from service worker.');
+      sendStatusUpdate(evt.ports && evt.ports[0]);
+    }
+  }
+
+  function sendStatusUpdate(target) {
+    sendSWMessage({ statusUpdate: { isOnline, isLoggedIn } }, target);
+  }
+
+  function sendSWMessage(msg, target) {
+    if (target) {
+      target.postMessage(msg);
+    } else if (svcworker) {
+      svcworker.postMessage(msg);
+    } else {
+      navigator.serviceWorker.controller.postMessage(msg);
+    }
+  }
 })();
--- service-worker-exercise/web/js/sw.js
+++ service-worker-exercise/web/js/sw.js
@@ -1,3 +1,47 @@
-"use strict";
+'use strict';
 
-// TODO
+const version = 2;
+isOnline = true;
+isLoggedIn = false;
+
+self.addEventListener('install', onInstall);
+self.addEventListener('activate', onActivate);
+self.addEventListener('message', onMessage);
+
+main().catch(console.error);
+
+async function main() {
+  await sendMessage({ requestStatusUpdate: true });
+}
+
+async function onInstall(evt) {
+  console.log(`Service worker ${version} is installed.`);
+  self.skipWaiting();
+}
+
+async function sendMessage(msg) {
+  var allClients = await clients.matchAll({ includeUncontrolled: true });
+  return Promise.all(
+    allClients.map(function clientMsg(client) {
+      var chan = new MessageChannel();
+      chan.port1.onmessage = onMessage;
+      return client.postMessage(msg, [chan.port2]);
+    })
+  );
+}
+
+function onMessage({ data }) {
+  if (data.statusUpdate) {
+    ({ isOnline, isLoggedIn } = data.statusUpdate);
+    console.log(`Service Worker v${version} status update`);
+  }
+}
+
+function onActivate(evt) {
+  evt.waitUntil(handleActivation());
+}
+
+async function handleActivation() {
+  await clients.claim();
+  console.log(`Service worker ${version} is activated.`);
+}
